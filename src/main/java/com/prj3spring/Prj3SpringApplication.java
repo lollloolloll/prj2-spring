7a88ac7b67d752499b9f250032020397cf012451
ganghyeon jo <ganghyeon4479@naver.com>
2024-05-21 10:13:29 +0900

    최초커밋

---
+++ src/main/java/com/prj3spring/Prj3SpringApplication.java
@@ -0,0 +1,13 @@
+package com.prj3spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj3SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj3SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prj3spring/Prj3SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prj3spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj3SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
